ba0298aaeeec2014a9f89a838806ab43d2ce347e
santiago-rodrig <santo1996.29@gmail.com>
2021-02-18 09:02:52 -0400

    refactor used errors and fix dictionary.Add

--- dictionary_test.go
+++ dictionary_test.go
@@ -20,12 +20,25 @@ func TestSearch(t *testing.T) {
 }
 
 func TestAdd(t *testing.T) {
-	dictionary := Dictionary{}
-	word := "test"
-	dictionary.Add(word, "this is just a test")
-	definition := "this is just a test"
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "test"
+		err := dictionary.Add(word, "this is just a test")
+		definition := "this is just a test"
 
-	assertDefinition(t, dictionary, word, definition)
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, "new test")
+
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
 }
 
 func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
